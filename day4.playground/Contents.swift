3eb66cec14345a33fb73eecd75a5a631ecb862c7
Mia <mia@Apples-MacBook-Pro-30.local>
2019-08-08 11:02:20 -0400

    Day 4

---
+++ day4.playground/Contents.swift
@@ -0,0 +1,19 @@
+//Day 4 Warmup
+//Using if, else if, and else
+var favDrink = "Starbucks"
+
+if favDrink == "Starbucks" {
+    print("Starbucks is pretty good!")
+}
+else if favDrink == "Dunkin Donuts" {
+    print("Dunkin Donuts is pretty good too")
+}
+else {
+    print("I'm not so sure about this")
+}
+//Practice with for in loops
+var favFoods = ["pizza" , "pasta" , "ice cream" , "cookies" , "chips"]
+for favFood in favFoods {
+    print("I think that \(favFood) is really good")
+}
+
